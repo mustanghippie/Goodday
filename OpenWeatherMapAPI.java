68251fd09d7ac52950099ce14a7b057678a1bd91
Aleksandra Korolczuk <korolczulaleksandra@gmail.com>
2017-05-04 12:34:24 -0700

    Created hash maps with weather data for now, in 3hours, in 6 hours, in 9 hours (OpenWeatheMapAPI) to fix: getting access to single value temp and weather in Controller

--- OpenWeatherMapAPI.java
+++ OpenWeatherMapAPI.java
@@ -46,11 +46,11 @@ public class OpenWeatherMapAPI {
         }
     }
 
-    public HashMap<String,String> openWeatherMap(){
+    public  HashMap<String, Object> openWeatherMap(){
         // VancouverID 6173331
         String requestURL = "http://api.openweathermap.org/data/2.5/forecast?id=6173331&APPID="+this.getApiKey();
         String data="";
-        HashMap<String,String> resultWeatherInformation = new HashMap<>();
+        HashMap<String,Object> allWeatherData = new HashMap<>();
 
         // Get weather information from API
         try {
@@ -78,20 +78,55 @@ public class OpenWeatherMapAPI {
             while((str = br.readLine()) != null){
                 data += str;
             }
-
             /** /---Debug code--- */
 
             JSONObject jsonObject = JSONObject.fromObject(data);
             //System.out.println(jsonObject);
             JSONArray listArray = jsonObject.getJSONArray("list");
-            JSONObject jobj = listArray.getJSONObject(0);
-            //System.out.println(jobj.getJSONArray("weather").getJSONObject(0));
+            //JSONObject jobj = listArray.getJSONObject(0);
 
-            String weather = jobj.getJSONArray("weather").getJSONObject(0).getString("main");
-            String temp = String.format("%.2f", jobj.getJSONObject("main").getDouble("temp") - 273.15f) +"°";
+            HashMap<String, String> nowWeatherData =new HashMap<>();
+            HashMap<String, String> in3WeatherData =new HashMap<>();
+            HashMap<String, String> in6WeatherData =new HashMap<>();
+            HashMap<String, String> in9WeatherData =new HashMap<>();
+            JSONObject jobj;
 
-            resultWeatherInformation.put("weather",weather);
-            resultWeatherInformation.put("temp",temp);
+            for (int i=0;i<4;i++) {
+                jobj = listArray.getJSONObject(i);
+
+                //System.out.println(jobj.getJSONArray("weather").getJSONObject(0));
+
+                String weather = jobj.getJSONArray("weather").getJSONObject(0).getString("main");
+                String temp = String.format("%.2f", jobj.getJSONObject("main").getDouble("temp") - 273.15f) + "°";
+
+                switch (i){
+                    case 0:
+                        nowWeatherData.put("weather", weather);
+                        nowWeatherData.put("temp", temp);
+                        break;
+                    case 1:
+                        in3WeatherData.put("weather", weather);
+                        in3WeatherData.put("temp", temp);
+                        break;
+                    case 2:
+                        in6WeatherData.put("weather", weather);
+                        in6WeatherData.put("temp", temp);
+                        break;
+                    case 3:
+                        in9WeatherData.put("weather", weather);
+                        in9WeatherData.put("temp", temp);
+                        break;
+                    default:
+                        System.out.println("Ouf of index");
+
+                }
+            }
+
+            allWeatherData.put("now", nowWeatherData);
+            allWeatherData.put("in3", in3WeatherData);
+            allWeatherData.put("in6", in6WeatherData);
+            allWeatherData.put("in9", in9WeatherData);
+
 
 
             // Save weather information(JSON)
@@ -111,7 +146,9 @@ public class OpenWeatherMapAPI {
             e.printStackTrace();
         }
 
-        return resultWeatherInformation;
+
+        return allWeatherData;
+
 
     }
 
