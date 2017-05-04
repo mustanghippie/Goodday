06537110ca965dc074732570dd598bb9d5f2dd81
paulomelila <paulovitor22@hotmail.com>
2017-05-04 12:34:47 -0700

    Setting Page

--- SettingPageController.java
+++ SettingPageController.java
@@ -3,9 +3,12 @@ package goodday;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import org.controlsfx.control.textfield.TextFields;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -15,6 +18,13 @@ import java.util.ResourceBundle;
 public class SettingPageController extends AnchorPane implements Initializable{
 
     private GoodDayModel gdm = new GoodDayModel();
+
+    @FXML
+    TextField locationName;
+    @FXML
+    RadioButton convertToFahrenheit;
+    @FXML
+    RadioButton convertToCelsius;
 
     public SettingPageController(){
 
@@ -36,16 +46,6 @@ public class SettingPageController extends AnchorPane implements Initializable{
         }
     }
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-    }
-
-    //----------------------------------
-    //  Button to change input location
-    //----------------------------------
-    @FXML
-    protected void changeLocation() {
-
 
 
 
@@ -60,9 +60,13 @@ public class SettingPageController extends AnchorPane implements Initializable{
      */
     @FXML
     protected void convertToFahrenheit() {
-
+        /*
+        if (unit == 1) // if input unit is celsius (unit 1)
+        {
+            //  convert to fahrenheit (unit 2)
+        }
+        */
     }
-
 
 
     /** CONVERT TO CELSIUS BUTTON
@@ -75,6 +79,14 @@ public class SettingPageController extends AnchorPane implements Initializable{
      */
     @FXML
     protected void convertToCelsius () {
+
+        /*
+        if (unit == 2)  // if input unit is fahrenheit (unit 2)
+        {
+            //  convert to celsius (unit 1)
+        }
+        */
+
     }
 
 
@@ -88,8 +100,41 @@ public class SettingPageController extends AnchorPane implements Initializable{
      */
     @FXML
     protected void applyChanges() {
+        //System.out.println(locationName.getText());
+        String location = locationName.getText();
+
+        try {
+            gdm.setUserSetting(location,2);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("--- IOException|@applyChanges---");
+        }
 
     }
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // suggestion function
+        String[] suggestion = new String[35586];
 
+        try {
+            File file = new File("src/goodday/CityData/city_name_list");
+            FileReader filereader = new FileReader(file);
+            BufferedReader br = new BufferedReader(filereader);
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < suggestion.length; i++) {
+                suggestion[i] = br.readLine();
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("FileNotFoundException");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("IOException");
+        }
+        TextFields.bindAutoCompletion(locationName, suggestion);
+    }
 }
