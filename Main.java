5101aecd3d704ba0bbb25103fcfb26f11b5c229e
Musta <musta@XGP-M.local>
2017-05-09 10:50:08 -0700

    setResizable(false)

--- Main.java
+++ Main.java
@@ -25,6 +25,7 @@ public class Main extends Application {
         stage = primaryStage;
         stage.setWidth(1280);
         stage.setHeight(800);
+        stage.setResizable(false);
 
         // If user's setting doesn't exist, this skips Register Location Page
         if(!checkUserExists()) this.sendRegisterLocationPage();
